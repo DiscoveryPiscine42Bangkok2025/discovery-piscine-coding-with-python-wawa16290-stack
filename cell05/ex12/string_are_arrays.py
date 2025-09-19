228e82b9c176368ba434e4185f371e851ca1255e
wawa16290-stack <wawa16290@gmail.com>
2025-09-19 10:57:23 +0000

    cell05-ex12

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    input_str = sys.argv[1]
+    z_count == 0:
+    if z_count == 0:
+        print("none")
+    else:
+        print("z" * z_count)
